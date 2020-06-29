1eff9888c5eef9af09040d58d85b04bcd3572806
truizdiaz <tomiruizdiaz@gmail.com>
2020-06-29 12:13:16 -0300

    Dashboard Component

---
+++ src/app/dashboard/list-tareas/list-tareas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-tareas',
+  templateUrl: './list-tareas.component.html',
+  styleUrls: ['./list-tareas.component.css']
+})
+export class ListTareasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
